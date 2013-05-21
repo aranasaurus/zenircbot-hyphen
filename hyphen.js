51e29bbf04dcb5157e73cf87741bc578096962bf
Ryan Arana <ryan.arana@gmail.com>
2013-05-21 14:46:21 -0700

    Add more flexibility to the regex

--- hyphen.js
+++ hyphen.js
@@ -9,7 +9,7 @@ sub.on('message', function( channel, message ) {
     if (msg.version == 1) {
         if (msg.type == 'privmsg') {
             var match;
-            var regex = /^.*(that|those)(?:'?(s|re))? (is|are)? ?(a|an|some) (\w+)[\- ](ass) ([^!.$]+)(.*)$/i;
+            var regex = /^.*(that|those|it)(?:'?(s|re))? (is|are)? ?(a|an|some) ?\w* (\w+)[\- ](ass) ([^!.$]+)(.*)$/i;
             if (match=regex.exec(msg.data.message)) {
                 var demonstrative = match[1],
                     verb = match[2] || match[3],
@@ -23,13 +23,17 @@ sub.on('message', function( channel, message ) {
                 } else if (verb == "re") {
                     verb = "are";
                 }
+                if (demonstrative == "it") {
+                    demonstrative = "that";
+                }
                 var responses = [
                     "wow, " + demonstrative + " *" + verb + "* " + article + " " + adjective + " " + ass + "-" + noun + punctuation,
                     "dude, you're right, " + demonstrative + " " + ass + "-" + noun + " " + verb + " totally " + adjective + punctuation,
                     "yeah " + demonstrative + " " + verb + " " + article + " " + adjective + " " + ass + "-" + noun + punctuation,
-                    demonstrative + " " + verb + " " + article + " " + adjective + " " + ass + "-" + noun + punctuation,
+                    demonstrative + " really " + verb + " " + article + " " + adjective + " " + ass + "-" + noun + punctuation,
                     "I honestly can't believe just how " + adjective + " " + demonstrative + " " + ass + "-" + noun + " " + verb + punctuation
                 ];
+
                 zen.send_privmsg(msg.data.channel, responses[Math.floor(Math.random() * responses.length)]);
             }
         }
