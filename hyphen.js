faff7b137a62b6e2eaa46724cefbef09576f6443
Ryan Arana <ryan.arana@gmail.com>
2013-06-24 14:19:04 -0700

    Add these as a possible for the noun slot, and make the article slot optional.

--- hyphen.js
+++ hyphen.js
@@ -9,15 +9,18 @@ sub.on('message', function( channel, message ) {
     if (msg.version == 1) {
         if (msg.type == 'privmsg') {
             var match;
-            var regex = /^.*(that|those|it)(?:'?(s|re))? (is|are)? ?(a|an|some) ?\w* (\w+)[\- ](ass) ([^!.$]+)(.*)$/i;
+            var regex = /^.*(that|those|these|it)(?:'?(s|re))? (is|are)? ?(a|an|some)? ?\w* (\w+)[\- ](ass) ([^!.$]+)(.*)$/i;
             if (match=regex.exec(msg.data.message)) {
                 var demonstrative = match[1],
                     verb = match[2] || match[3],
-                    article = match[4],
+                    article = match[4] || "",
                     adjective = match[5],
                     ass = match[6],
                     noun = match[7],
                     punctuation = match[8];
+                if (article) {
+                    article += " ";
+                }
                 if (verb == "s") {
                     verb = "is";
                 } else if (verb == "re") {
@@ -25,12 +28,14 @@ sub.on('message', function( channel, message ) {
                 }
                 if (demonstrative == "it") {
                     demonstrative = "that";
+                } else if (demonstrative == "these") {
+                    demonstrative = "those";
                 }
                 var responses = [
-                    "wow, " + demonstrative + " *" + verb + "* " + article + " " + adjective + " " + ass + "-" + noun + punctuation,
+                    "wow, " + demonstrative + " *" + verb + "* " + article + adjective + " " + ass + "-" + noun + punctuation,
                     "dude, you're right, " + demonstrative + " " + ass + "-" + noun + " " + verb + " totally " + adjective + punctuation,
-                    "yeah " + demonstrative + " " + verb + " " + article + " " + adjective + " " + ass + "-" + noun + punctuation,
-                    demonstrative + " really " + verb + " " + article + " " + adjective + " " + ass + "-" + noun + punctuation,
+                    "yeah " + demonstrative + " " + verb + " " + article + adjective + " " + ass + "-" + noun + punctuation,
+                    demonstrative + " really " + verb + " " + article + adjective + " " + ass + "-" + noun + punctuation,
                     "I honestly can't believe just how " + adjective + " " + demonstrative + " " + ass + "-" + noun + " " + verb + punctuation
                 ];
 
